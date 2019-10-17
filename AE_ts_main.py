f367d96b9a42625d31ea0708bae533d9e9f12de2
Bery Saidi <abdelberysaidi@gmail.com>
2019-10-16 22:01:33 -0400

    cleanup 0.1

--- AE_ts_main.py
+++ AE_ts_main.py
@@ -4,7 +4,7 @@ Created on Tue Mar 22 10:43:29 2016
 
 @author: Rob Romijnders
 """
-
+import argparse
 import numpy as np
 import tensorflow as tf
 import os
@@ -15,9 +15,7 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 
 """Hyperparameters"""
 # Please download data here: https://www.cs.ucr.edu/~eamonn/time_series_data/
-# and set direc to the location of the data directory
-direc = '/home/rob/Dropbox/ml_projects/LSTM/UCR_TS_Archive_2015'
-LOG_DIR = "/home/rob/Dropbox/ml_projects/AE_ts/log_tb"  # Directory for the logging
+LOG_DIR = "./foo.log"  # Directory for the logging
 
 
 config = dict()  # Put all configuration information into the dict
@@ -29,84 +27,56 @@ config['learning_rate'] = .005
 config['crd'] = 1  # Hyperparameter for future generalization
 config['num_l'] = 20  # number of units in the latent space
 
-plot_every = 100  # after _plot_every_ GD steps, there's console output
-max_iterations = 1000  # maximum number of iterations
-dropout = 0.8  # Dropout rate
+
+#change me
+datapath = './ecg_data.csv'
+#constant bullshit
+dataset="ECG5000"
+datadir = datapath + '/' + dataset + '/' + dataset
+
+#real input should be this
+test_path = datadir + '_TEST'
+train_path = datadir + '_TRAIN'
+
+def api_open_data(test_p, train_p, ratio_train=0.8):
+    """Input:
+    ratio_train: ratio to split training and testset
+    """
+    data_test_val = np.loadtxt(test_p, delimiter=',')[:-1]
+    data_train = np.loadtxt(train_p, delimiter=',')
+
+    data = np.concatenate((data_train, data_test_val), axis=0)
+
+    N, D = data.shape
+
+    ind_cut = int(ratio_train * N)
+    ind = np.random.permutation(N)
+    return data[ind[:ind_cut], 1:], data[ind[ind_cut:], 1:], data[ind[:ind_cut], 0], data[ind[ind_cut:], 0]
 
 
+
+
+
+
+def api_call(config)
+     
 # Load the data
-X_train, X_val, y_train, y_val = open_data('/home/rob/Dropbox/ml_projects/LSTM/UCR_TS_Archive_2015')
+    X_train, X_val, y_train, y_val = api_open_data(test_path, train_path, 0.8)
 
-N = X_train.shape[0]
-Nval = X_val.shape[0]
-D = X_train.shape[1]
-config['sl'] = sl = D  # sequence length
-print('We have %s observations with %s dimensions' % (N, D))
+    Nval = X_val.shape[0]
+    D = X_train.shape[1]
 
-# Organize the classes
-num_classes = len(np.unique(y_train))
-base = np.min(y_train)  # Check if data is 0-based
-if base != 0:
-    y_train -= base
-    y_val -= base
 
-# Plot data   # and save high quality plt.savefig('data_examples.eps', format='eps', dpi=1000)
-do_plot_example = False
-if do_plot_example:
-    plot_data(X_train, y_train)
+    config['sl'] = sl = D  # sequence length
+    print('We have %s observations with %s dimensions' % (N, D))
 
-"""Training time!"""
-model = Model(config)
-sess = tf.Session()
-perf_collect = np.zeros((2, int(np.floor(max_iterations / plot_every))))
-do_train = False
-if do_train:
-
-    # Proclaim the epochs
-    epochs = np.floor(batch_size * max_iterations / N)
-    print('Train with approximately %d epochs' % epochs)
-
-    sess.run(model.init_op)
-    writer = tf.summary.FileWriter(LOG_DIR, sess.graph)  # writer for Tensorboard
-
-    step = 0  # Step is a counter for filling the numpy array perf_collect
-    for i in range(max_iterations):
-        batch_ind = np.random.choice(N, batch_size, replace=False)
-        result = sess.run([model.loss, model.loss_seq, model.loss_lat_batch, model.train_step],
-                          feed_dict={model.x: X_train[batch_ind], model.keep_prob: dropout})
-
-        if i % plot_every == 0:
-            # Save train performances
-            perf_collect[0, step] = loss_train = result[0]
-            loss_train_seq, lost_train_lat = result[1], result[2]
-
-            # Calculate and save validation performance
-            batch_ind_val = np.random.choice(Nval, batch_size, replace=False)
-
-            result = sess.run([model.loss, model.loss_seq, model.loss_lat_batch, model.merged],
-                              feed_dict={model.x: X_val[batch_ind_val], model.keep_prob: 1.0})
-            perf_collect[1, step] = loss_val = result[0]
-            loss_val_seq, lost_val_lat = result[1], result[2]
-            # and save to Tensorboard
-            summary_str = result[3]
-            writer.add_summary(summary_str, i)
-            writer.flush()
-
-            print("At %6s / %6s train (%5.3f, %5.3f, %5.3f), val (%5.3f, %5.3f,%5.3f) in order (total, seq, lat)" % (
-            i, max_iterations, loss_train, loss_train_seq, lost_train_lat, loss_val, loss_val_seq, lost_val_lat))
-            step += 1
-
-    # Save the model
-    saver = tf.train.Saver()
-    saver.save(sess, os.path.join(LOG_DIR, "model.ckpt"), step)
-
-else:
-    # Load from saved model
+    """Training time!"""
+    model = Model(config)
+    sess = tf.Session()
+    perf_collect = np.zeros((2, int(np.floor(max_iterations / plot_every))))
     saver = tf.train.Saver()
     saver.restore(sess, save_path=os.path.join(LOG_DIR, "model.ckpt-10"))
 
-do_plot = True
-if do_plot:
     # Extract the latent space coordinates of the validation set
     start = 0
     label = []  # The label to save to visualize the latent space
@@ -123,6 +93,11 @@ if do_plot:
 
     plot_z_run(z_run, label)
 
-sess.close()
-# Now open Tensorboard with
-#  $tensorboard --logdir = LOG_DIR
+    sess.close()
+
+    return z_run
+   
+
+if __name__ == '__main__':
+
+    result = api_call(config)
